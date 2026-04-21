e1c965b5b36c3de47c87a0e17fffc6cab7e7270b
Mohamed Walid Elnabawy <155386288+MohamedWalidElnabawy@users.noreply.github.com>
2026-04-21 17:37:21 +0200

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        freq = [0] * 26
+
+        for char in s:
+            freq[ord(char) - ord('a')] += 1
+        for char in t:
+            freq[ord(char) - ord('a')] -= 1
+        return all(x == 0 for x in freq)
